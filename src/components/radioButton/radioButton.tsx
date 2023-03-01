d65e4bd242472816536aa1f776cbd0f6c6bc39f2
chimichi004 <chember004@gmail.com>
2023-03-01 23:33:03 +0800

    add on click on the box component

--- src/components/radioButton/radioButton.tsx
+++ src/components/radioButton/radioButton.tsx
@@ -9,10 +9,21 @@ export const RadioButton = () => {
     const [textDescription, setTextDescription] = useState<string>('professionally constructed multi-sector investment options');
     const [name, setName] = useState<string>('investmentStrategy');
     const [value, setValue] = useState<string>('tailormade');
+    const [sourceImg, setSourceImg] = useState<string>('https://robohash.org/1?set=set2');
+    const [altImg, setAltImg] = useState<string>('sample-image');
+    const [radioToggle, setRadioToggle] = useState(false)
+
+    const radioToggleEvent = () => {
+      console.log('is clicked')
+      setRadioToggle(!radioToggle);
+      setValue(`${radioToggle}`);
+    }
+    
+
     return (
         <RadioButtonDiv>
              <Radio name={name} value={value}/>
-             <RadioButtonBox textDescription={textDescription} titlePrimary={titlePrimary}/>
+             <RadioButtonBox ontoggle={radioToggleEvent} textDescription={textDescription} titlePrimary={titlePrimary} sourceImg={sourceImg} altImg={altImg}/>
         </RadioButtonDiv>
        
       );
